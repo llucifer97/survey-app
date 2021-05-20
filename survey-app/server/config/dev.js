b88b3ef0026a4e0945847975a8d4bd446c46c068
ayush raj <ayushrajpoot97@gmail.com>
2021-05-20 19:49:15 +0530

    Add files via upload

---
+++ survey-app/server/config/dev.js
@@ -0,0 +1,15 @@
+// dev.js do n ot commit this!
+
+
+module.exports = {
+  googleClientID : '',
+  googleClientSecret : '',
+  mongoURI : '',
+  cookieKey: '',
+  stripePublishableKey: ,
+   stripeSecretKey: '',
+sendGridKey: '',
+redirectDomain: 'http://localhost:3000'
+}
+
+
---
+++ survey-app/server/sendgrid-test/test.js
@@ -0,0 +1,14 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey('');
+const msg = {
+  to: "ayushraj.bit17@gmail.com",
+  from: "ayushrajpoot97@gmail.com",
+  subject: "Sending with Twilio SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+sgMail.send(msg);
+
+
